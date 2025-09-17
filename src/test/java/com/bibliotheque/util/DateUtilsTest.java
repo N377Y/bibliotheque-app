723fe8338a3a41c18345039e16994d95f28ea864
Nelly <marie-ange.kuitche@groupe-esigelec.org>
2025-09-17 15:35:41 +0200

    seventh commit

---
+++ src/test/java/com/bibliotheque/util/DateUtilsTest.java
@@ -0,0 +1,13 @@
+package com.bibliotheque.util;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DateUtilsTest {
+
+    @Test
+    void sanity() {
+        // Test trivial pour vérifier que la stack de tests fonctionne
+        assertTrue(1 + 1 == 2);
+    }
+}
